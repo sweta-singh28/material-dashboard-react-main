b7fa9380e2609828ad6e32ed99635b28feec1964
sweta-singh28 <swetasinghgs28@gmail.com>
2025-09-07 20:33:43 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -179,7 +179,7 @@ export default function App() {
           <Sidenav
             color={sidenavColor}
             brand={(transparentSidenav && !darkMode) || whiteSidenav ? brandDark : brandWhite}
-            brandName="ABC"
+            brandName="Coursify"
             routes={routes}
             onMouseEnter={handleOnMouseEnter}
             onMouseLeave={handleOnMouseLeave}
